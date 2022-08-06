40baa5ae074a23226ecc85fd1d58ca6d60cdf010
William Queiroz <wnqueiroz@outlook.com>
2022-08-05 22:31:29 -0300

    feat: add HTML structure

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,48 @@
+const styles = {
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    width: 500,
+    height: 500,
+    display: "flex",
+    flexDirection: "column",
+  },
+};
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  // 3. Criamos a função que irá lidar com o envio do nosso formulário
+  function onSubmit(e) {
+    e.preventDefault();
+
+    console.log("Enviou os dados da mensagem");
+  }
+
+  return (
+    <div style={styles.root}>
+      {/* 2. Criamos a estrutura do noss HTML + os estilos */}
+      <div style={styles.container}>
+        <h1>Chat</h1>
+
+        <div></div>
+
+        <form onSubmit={onSubmit}>
+          <input type="text" name="mensagem" />
+          <input type="submit" value="Enviar mensagem" />
+        </form>
+      </div>
+
+      {/* 1. Criamos os estilos globais para centralizar o conteúdo na página */}
+      <style global jsx>{`
+        html,
+        body,
+        div#__next,
+        div#__next > div {
+          height: 100%;
+        }
+      `}</style>
+    </div>
+  );
 }
